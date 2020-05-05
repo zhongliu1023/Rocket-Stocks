e0b00444ce372371836e57783e004e99b939f721
Zhu Zhen <g.hijack333@gmail.com>
2020-05-06 05:57:49 +0800

    Update: add interval option

--- src/DetailedStock/index.js
+++ src/DetailedStock/index.js
@@ -1,9 +1,26 @@
 import React, { useEffect } from "react";
 import TradingViewWidget, { Themes } from "react-tradingview-widget";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const queryOrDefault = (query, attr, def) => {
+  return query.get(attr) !== null ? query.get(attr) : def;
+};
+
+const intervalRangeMap = {
+  A: "6m",
+  D: "12m",
+  W: "60m",
+  M: "120m",
+};
 
 export default (props) => {
   const { symbol } = useParams();
+  const query = useQuery();
+  const interval = queryOrDefault(query, "interval", "D");
 
   useEffect(() => {
     var height =
@@ -18,8 +35,9 @@ export default (props) => {
       <TradingViewWidget
         symbol={`NASDAQ:${symbol}`}
         theme={Themes.DARK}
-        withdateranges={true}
-        hide_side_toolbar={false}
+        interval={interval}
+        range={intervalRangeMap[interval]}
+        hide_side_toolbar={true}
         autosize
       />
     </div>
