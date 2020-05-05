eb73f993fcf32295a0001c50f70d746d2648aa52
Zhu Zhen <g.hijack333@gmail.com>
2020-05-05 23:33:38 +0800

    Updates

---
+++ precache-manifest.415b15902e0b448b4fe59cdab805837d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b9be32a96dad1be52a5f0e7b72bcbb",
+    "url": "/Rocket-Stocks/index.html"
+  },
+  {
+    "revision": "b3c64749ff3c37948dc4",
+    "url": "/Rocket-Stocks/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1ea8ae33ee23f7db8229",
+    "url": "/Rocket-Stocks/static/js/2.740aca9d.chunk.js"
+  },
+  {
+    "revision": "9717c5652d81088c1cd59929c178594d",
+    "url": "/Rocket-Stocks/static/js/2.740aca9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c64749ff3c37948dc4",
+    "url": "/Rocket-Stocks/static/js/main.aa9d3058.chunk.js"
+  },
+  {
+    "revision": "4d5a112a133ce7e75fa3",
+    "url": "/Rocket-Stocks/static/js/runtime-main.e4adf95f.js"
+  }
+]);
